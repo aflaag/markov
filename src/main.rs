b63fd0eadf72083f856d17be4c562838ab84e222
ph04 <tacc010203@gmail.com>
2022-08-15 17:22:06 +0200

    missing randomness

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::fs::read_to_string;
 fn main() {
     let input = read_to_string("input.txt").expect("Something went wrong while trying to open the input file.");
 
-    let output = MarkovStates::<NGram<3>, 3>::from(input.as_bytes())
-    // let output = MarkovStates::<NGram<3>, 3>::from("abcbab".as_bytes())
+    let output = MarkovStates::<NGram<2>, 2>::from(input.as_bytes())
         .into_iter()
-        .take(10)
+        .take(100)
+        .map(|byte| byte as char)
         .collect::<String>();
 
     println!("{}", output);
--- src/markov.rs
+++ src/markov.rs
@@ -1,20 +1,30 @@
 use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
-use std::io::Read;
-use rand::Rng;
+// use std::io::Read;
+// use rand::Rng;
 
-pub trait State: Hash + Eq {}
+pub trait State: Hash + Eq + IntoIterator {}
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub struct NGram<const N: usize> {
     ngram: [u8; N],
 }
 
 impl<const N: usize> State for NGram<N> {}
 
+impl<const N: usize> IntoIterator for NGram<N> {
+    type Item = u8;
+    type IntoIter = std::array::IntoIter<u8, N>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.ngram.into_iter()
+    }
+}
+
 impl<const N: usize> ToString for NGram<N> {
     fn to_string(&self) -> String {
-        todo!()
+        println!("funziona");
+        String::from_utf8(self.ngram.to_vec()).unwrap()
     }
 }
 
@@ -30,35 +40,63 @@ impl<const N: usize> From<[u8; N]> for NGram<N> {
     }
 }
 
-pub struct MarkovIter<S: State> {
+pub struct MarkovIter<S: State, const N: usize> {
     prev_state: Option<S>,
+    states: HashMap<S, HashSet<u8>>,
 }
 
-impl<S: State> MarkovIter<S> {
-    pub fn new(first_state: Option<S>) -> Self {
-        Self { prev_state: first_state }
+impl<S: State, const N: usize> MarkovIter<S, N> {
+    pub fn new(first_state: Option<S>, states: HashMap<S, HashSet<u8>>) -> Self {
+        Self { prev_state: first_state, states }
     }
 }
 
-impl<S: State> Iterator for MarkovIter<S> {
-    type Item = S;
+impl<S, const N: usize> Iterator for MarkovIter<S, N>
+where
+    S: State + Clone + Copy + From<[u8; N]>,
+    <S as IntoIterator>::Item: Into<u8>,
+{
+    type Item = u8;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        if let Some(prev) = self.prev_state {
+            let next_states = self.states.get(&prev).unwrap();
+
+            let next_char = *next_states.iter().take(1).next().unwrap();
+
+            let mut new_prev_state = [0; N];
+
+            new_prev_state
+                .iter_mut()
+                .zip(prev.into_iter().skip(1))
+                .for_each(|(new_c, old_c)| *new_c = old_c.into());
+
+            new_prev_state[N - 1] = next_char;
+
+            self.prev_state = Some(new_prev_state.into());
+            
+            Some(next_char)
+        } else {
+            None
+        }
     }
 }
 
 #[derive(Clone)]
 pub struct MarkovStates<S: State, const N: usize>  {
-    states: HashMap<S, HashSet<S>>,
+    states: HashMap<S, HashSet<u8>>,
 }
 
-impl<S: State, const N: usize> IntoIterator for MarkovStates<S, N> {
-    type Item = S;
-    type IntoIter = MarkovIter<S>;
+impl<S, const N: usize> IntoIterator for MarkovStates<S, N>
+where
+    S: State + Clone + Copy + From<[u8; N]>,
+    <S as IntoIterator>::Item: Into<u8>,
+{
+    type Item = u8;
+    type IntoIter = MarkovIter<S, N>;
 
-    fn into_iter(self) -> MarkovIter<S> {
-        MarkovIter::new(self.states.into_keys().take(1).next())
+    fn into_iter(self) -> MarkovIter<S, N> {
+        MarkovIter::new(self.states.clone().into_keys().take(1).next(), self.states)
     }
 }
 
@@ -70,13 +108,12 @@ impl<S: State, const N: usize> Default for MarkovStates<S, N> {
 
 impl<S: std::fmt::Debug+ State + From<[u8; N]>, const N: usize> From<&[u8]> for MarkovStates<S, N> {
     fn from(chars: &[u8]) -> Self {
-        let mut states: HashMap<S, HashSet<S>> = HashMap::new();
+        let mut states: HashMap<S, HashSet<u8>> = HashMap::new();
 
         chars
-            .windows(N * 2)
+            .windows(N + 1)
             .for_each(|slice| {
                 let mut curr = [0; N];
-                let mut next = [0; N];
 
                 let mut bytes = slice.iter();
 
@@ -84,20 +121,15 @@ impl<S: std::fmt::Debug+ State + From<[u8; N]>, const N: usize> From<&[u8]> for 
                     .iter_mut()
                     .for_each(|byte| *byte = *bytes.next().unwrap());
 
-                next
-                    .iter_mut()
-                    .for_each(|byte| *byte = *bytes.next().unwrap());
-
                 let curr_state = S::from(curr);
-                let next_state = S::from(next);
+                let next_char = slice[N];
 
-                if let Some(next_states) = states.get_mut(&curr_state) {
-                    next_states.insert(next_state);
+                if let Some(next_chars) = states.get_mut(&curr_state) {
+                    next_chars.insert(next_char);
                 } else {
-                    states.insert(curr_state, HashSet::from([next_state]));
+                    states.insert(curr_state, HashSet::from([next_char]));
                 }
             });
-        println!("{:?}", states);
 
         Self { states }
     }
